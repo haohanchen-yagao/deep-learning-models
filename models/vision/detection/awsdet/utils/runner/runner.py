30163b884797476a0c0f4141c3dcd204408e2258
haohanchen-yagao <haohanchen1994@gmail.com>
2020-10-27 13:46:21 -0700

    modify tape

--- models/vision/detection/awsdet/utils/runner/runner.py
+++ models/vision/detection/awsdet/utils/runner/runner.py
@@ -291,9 +291,9 @@ class Runner(object):
                 loss = self.optimizer.get_scaled_loss(outputs['loss'])
             else:
                 loss = outputs['loss']
-        #tape = herring.DistributedGradientTape(tape)
         var_list = self.model.trainable_variables
-        tape = get_distributed_tape(tape) if self.world_size > 1 else tape
+        tape = herring.DistributedGradientTape(tape)
+        #tape = get_distributed_tape(tape) if self.world_size > 1 else tape
         grads = tape.gradient(loss, var_list)
         if self._amp_enabled:
             grads = self.optimizer.get_unscaled_gradients(grads)
@@ -343,7 +343,7 @@ class Runner(object):
             self._inner_iter = i
             self.call_hook('before_train_iter')
             print("run step for iter {}".format(i))
-            _ = get_barrier()
+            #_ = get_barrier()
             print("in step barrier got")
             outputs = self.run_train_step(data_batch)
             if self.broadcast: # broadcast once
